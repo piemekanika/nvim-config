b484cd73396309013b33ec73ba421d95397d5f49
piemekanika <piemekanika@hotmail.com>
2025-07-14 11:40:30 +0700

    added treesitter textobjects config

--- lua/plugins/nvim-treesitter-textobjects.lua
+++ lua/plugins/nvim-treesitter-textobjects.lua
@@ -1,7 +1,26 @@
 return {
 	"nvim-treesitter/nvim-treesitter-textobjects",
-    lazy = false,
-    init = function ()
-        require("nvim-treesitter.configs").setup({})
-    end
+	lazy = false,
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	init = function()
+		require("nvim-treesitter.configs").setup({
+			text_objects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@conditional.outer",
+						["ic"] = "@conditional.inner",
+						["ab"] = "@block.outer",
+						["ib"] = "@block.inner",
+						["ar"] = "@return.outer",
+						["ir"] = "@return.inner",
+					},
+				},
+			},
+		})
+	end,
 }
+
