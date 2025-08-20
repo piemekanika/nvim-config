95ece6ee9d0a7a8fd4c132e3e6f871af8c11f85a
piemekanika <piemekanika@hotmail.com>
2025-08-20 19:28:17 +0700

    added avante with bothub as provider

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -26,6 +26,7 @@ return {
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				typescript = { "prettierd", "prettier", stop_after_first = true },
 				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				svelte = { "prettierd", "prettier", stop_after_first = true },
 			},
 		})
 	end,
