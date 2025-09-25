b6acb8b010798f39bfb6ba97a7c43009334bdf74
piemekanika <piemekanika@hotmail.com>
2025-09-25 14:14:41 +0700

    chore

--- lua/plugins/ts-autotag.lua
+++ lua/plugins/ts-autotag.lua
@@ -5,15 +5,20 @@ return {
 	},
 	config = function()
 		require("nvim-ts-autotag").setup({
-			-- Default configuration (optional, customize as needed)
 			opts = {
 				-- Enable autotagging for specific filetypes
 				enable_close = true, -- Auto close tags
 				enable_rename = true, -- Auto rename paired tags
 				enable_close_on_slash = false, -- Auto close on </
 			},
-			-- Optionally, specify filetypes (defaults to html, xml, etc.)
-			filetypes = { "html", "xml", "jsx", "tsx", "vue", "svelte", "php" },
+			filetypes = {
+                "html",
+                "xml",
+                "jsx",
+                "tsx",
+                "vue",
+                "svelte",
+            },
 		})
 	end,
 }
