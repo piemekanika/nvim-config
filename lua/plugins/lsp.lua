aab921957cfea599da9d5743429c1d78bfcf3f65
piemekanika <piemekanika@hotmail.com>
2025-09-25 12:13:47 +0700

    moved to new lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,7 +16,7 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				-- TODO move it to local var
-				ensure_installed = { "lua_ls" }, -- Add any LSP servers you want auto-installed
+				ensure_installed = { "vtsls", "lua_ls" }, -- Add any LSP servers you want auto-installed
 			})
 		end,
 	},
@@ -29,39 +29,17 @@ return {
 			{ "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
 		},
 		config = function()
-			-- Basic LSP setup example for Lua
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
+			vim.lsp.config("lua_ls", {
 				settings = {
 					Lua = {
 						diagnostics = {
-							globals = { "vim" }, -- Recognize Vim globals
+							globals = { "vim" },
 						},
 					},
 				},
 			})
 
-			-- lspconfig.ts_ls.setup({
-			-- 	on_attach = function(client, bufnr)
-			-- 		local opts = { buffer = bufnr, remap = false }
-			-- 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			-- 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-			-- 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-			-- 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-			-- 	end,
-			-- 	filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-			-- })
-
-			lspconfig.vtsls.setup({
-				on_attach = function(client, bufnr)
-					local opts = { buffer = bufnr, remap = false }
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-				end,
-				filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-			})
+			vim.lsp.enable({ "vtsls", "lua_ls" })
 
 			-- Optional: Keymaps for LSP
 			vim.api.nvim_create_autocmd("LspAttach", {
@@ -76,3 +54,39 @@ return {
 		end,
 	},
 }
+
+-- old lsp config
+-- -- Basic LSP setup example for Lua
+-- local lspconfig = vim.lsp.config
+-- lspconfig.lua_ls.setup({
+-- 	settings = {
+-- 		Lua = {
+-- 			diagnostics = {
+-- 				globals = { "vim" }, -- Recognize Vim globals
+-- 			},
+-- 		},
+-- 	},
+-- })
+
+-- lspconfig.ts_ls.setup({
+-- 	on_attach = function(client, bufnr)
+-- 		local opts = { buffer = bufnr, remap = false }
+-- 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+-- 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+-- 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+-- 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+-- 	end,
+-- 	filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+-- })
+
+-- lspconfig.vtsls.setup({
+-- 	on_attach = function(client, bufnr)
+-- 		local opts = { buffer = bufnr, remap = false }
+-- 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+-- 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+-- 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+-- 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+-- 	end,
+-- 	filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+-- })
+--
