9270f7cf8fe14245d16052cf445c290359f1850a
piemekanika <piemekanika@hotmail.com>
2026-07-31 11:36:08 +0700

    fix: enabled svelte treesitter integration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -51,60 +51,12 @@ return {
 				vim.treesitter.start()
 			end,
 		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "svelte",
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
 	end,
 }
-
--- return {
--- 	"nvim-treesitter/nvim-treesitter",
--- 	build = ":TSUpdate",
--- 	lazy = false,
--- 	config = function()
--- 		require("nvim-treesitter.configs").setup({
--- 			ensure_installed = {
--- 				"lua",
--- 				"go",
--- 				"gowork",
--- 				"gomod",
--- 				"gosum",
--- 				"gotmpl",
--- 				"comment",
--- 				"markdown",
--- 				"markdown_inline",
--- 				"javascript",
--- 				"typescript",
--- 				"yaml",
--- 				"svelte",
--- 				"proto",
--- 				"graphql",
--- 				"nginx",
--- 				"dockerfile",
--- 				"bash",
--- 				"gitignore",
--- 				"html",
--- 				"css",
--- 				"scss",
--- 			},
---
--- 			modules = {},
---
--- 			-- Automatically install parsers when entering a buffer (optional)
--- 			auto_install = true,
---
--- 			-- List of parsers to ignore installing (or "all")
--- 			ignore_install = {},
---
--- 			-- Install parsers synchronously (only applied to `ensure_installed`)
--- 			sync_install = false,
---
--- 			-- Enable syntax highlighting
--- 			highlight = {
--- 				enable = true,
--- 			},
---
--- 			-- Optional: Enable other Tree-sitter features like indentation or incremental selection
--- 			indent = {
--- 				enable = true,
--- 			},
--- 		})
--- 	end,
--- }
