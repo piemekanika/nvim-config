538a84156bad1501a30a2dbf99f71868e5ab4c89
piemekanika <piemekanika@hotmail.com>
2026-08-06 11:56:11 +0700

    commented go.nvim plugin

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,31 +1,31 @@
 return {
-	"ray-x/go.nvim",
-	dependencies = { -- optional packages
-		"ray-x/guihua.lua",
-		"neovim/nvim-lspconfig",
-		"theHamsta/nvim-dap-virtual-text",
-		"rcarriga/nvim-dap-ui",
-		"mfussenegger/nvim-dap",
-		-- { "nvim-treesitter/nvim-treesitter", branch = 'main' } -- optional for master version
-	},
-	opts = function()
-		require("go").setup({
-			lsp_cfg = true,
-		})
-		local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*.go",
-			callback = function()
-				require("go.format").goimports()
-			end,
-			group = format_sync_grp,
-		})
-		return {
-			-- lsp_keymaps = false,
-			-- other options
-		}
-	end,
-	event = { "CmdlineEnter" },
-	ft = { "go", "gomod" },
-	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	-- "ray-x/go.nvim",
+	-- dependencies = { -- optional packages
+	-- 	"ray-x/guihua.lua",
+	-- 	"neovim/nvim-lspconfig",
+	-- 	"theHamsta/nvim-dap-virtual-text",
+	-- 	"rcarriga/nvim-dap-ui",
+	-- 	"mfussenegger/nvim-dap",
+	-- 	-- { "nvim-treesitter/nvim-treesitter", branch = 'main' } -- optional for master version
+	-- },
+	-- opts = function()
+	-- 	require("go").setup({
+	-- 		lsp_cfg = true,
+	-- 	})
+	-- 	local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+	-- 	vim.api.nvim_create_autocmd("BufWritePre", {
+	-- 		pattern = "*.go",
+	-- 		callback = function()
+	-- 			require("go.format").goimports()
+	-- 		end,
+	-- 		group = format_sync_grp,
+	-- 	})
+	-- 	return {
+	-- 		-- lsp_keymaps = false,
+	-- 		-- other options
+	-- 	}
+	-- end,
+	-- event = { "CmdlineEnter" },
+	-- ft = { "go", "gomod" },
+	-- build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
